b50b838b67305a6aae863db6eb220a45c7bf4ccc
Richard Trygg Svartling <tryggrichard@gmail.com>
2025-11-24 08:33:59 +0100

    Initial commit

---
+++ Another Good Forum/Pages/CreateForum.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Another_Great_Forum.Pages.Shared
+{
+    public class CreateForumModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Another Good Forum/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Another_Great_Forum.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
