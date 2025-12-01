8322b12e1d5291537f80eb1ae1f587c2fe2ad685
Rohullah <rohullah.rezayi@campusnykoping.se>
2025-12-01 14:39:03 +0100

    Implemented Cookies for login

--- Another Good Forum/Pages/Login.cshtml.cs
+++ Another Good Forum/Pages/Login.cshtml.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
 
 namespace Another_Great_Forum.Pages
 {
@@ -8,21 +11,28 @@ namespace Another_Great_Forum.Pages
     {
         private readonly HttpClient _httpClient;
 
+        // Use a CookieContainer to store cookies
+        private readonly CookieContainer _cookieContainer;
 
-        public LoginModel(HttpClient httpClient)
+        public LoginModel()
         {
-            _httpClient = httpClient;
+            _cookieContainer = new CookieContainer();
+            var handler = new HttpClientHandler
+            {
+                CookieContainer = _cookieContainer,
+                UseCookies = true
+            };
+            _httpClient = new HttpClient(handler);
         }
 
-        public class InputModel {
-
+        public class InputModel
+        {
             [Required, EmailAddress]
             public string Email { get; set; }
 
             [Required]
             public string Password { get; set; }
-                
-        };
+        }
 
         [BindProperty]
         public InputModel Input { get; set; }
@@ -35,15 +45,26 @@ namespace Another_Great_Forum.Pages
                 Password = Input.Password
             };
 
+            // Send login request
             var response = await _httpClient.PostAsJsonAsync("https://localhost:7242/users/login", requestBody);
 
             if (response.IsSuccessStatusCode)
             {
+                // Get cookies set by the API
+                var cookies = _cookieContainer.GetCookies(new Uri("https://localhost:7242"));
+
+                foreach (Cookie cookie in cookies)
+                {
+                    // Optional: log cookie names for debugging
+                    Console.WriteLine($"{cookie.Name} = {cookie.Value}");
+                }
+
+                // Now the cookie is stored in _cookieContainer
+                // Future requests using _httpClient will include the cookie automatically
                 return RedirectToPage("/Index");
             }
             else
             {
-                // Login failed, show error message
                 ModelState.AddModelError(string.Empty, "Invalid login attempt.");
                 return Page();
             }
