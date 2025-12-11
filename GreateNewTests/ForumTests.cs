1a0102fc609263039f94b89df0dbb9c5b1a76f65
Richard Trygg Svartling <tryggrichard@gmail.com>
2025-12-11 19:52:08 +0100

    Changed to inmemory

--- GreateNewTests/ForumTests.cs
+++ GreateNewTests/ForumTests.cs
@@ -44,6 +44,33 @@ namespace ForumTest
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.UseEnvironment("Testing");
+            
+            builder.ConfigureTestServices(services =>
+            {
+                // Remove the existing DbContext registration
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<ForumDbContext>));
+            
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                // Also remove the DbContext itself
+                var dbContextDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(ForumDbContext));
+                
+                if (dbContextDescriptor != null)
+                {
+                    services.Remove(dbContextDescriptor);
+                }
+            
+                // Add in-memory database
+                services.AddDbContext<ForumDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("TestDatabase");
+                });
+            });
         }
 
         public HttpClient CreateClientWithCookies()
